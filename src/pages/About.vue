537824382abf9e9c0b5feff2aef734d72e66d58e
Corky Brown <galaxyblur@gmail.com>
2019-05-23 22:48:05 -0400

    Update About text

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,16 @@
 <template>
   <q-page padding>
-    <em>Capoeira Letras</em> is the definitive lyrics resource for capoeiristas. Each song was
-    collected by hand and added to this library to ensure that you can find any lyric in one
-    place. While capoeira lyrics are often customized, tweaked, and improvised depending on
-    the vocalist, you should be able to find what you're looking for if you know even just a
-    few words. Enjoy!
+    <p>
+      <em>Capoeira Letras</em> is the ultimate lyrics resource for capoeiristas. Each song was
+      collected and added to this library to ensure that you can find any lyric in one place.
+    </p>
+    <p>
+      While capoeira lyrics are often customized, tweaked, and improvised depending on
+      the vocalist, you should be able to find what you're looking for if you know even just a
+      few words.
+    </p>
+    <p>
+      Enjoy!
+    </p>
   </q-page>
 </template>
